665b24c093dbeeca376da354db7f89138c0ea508
Vikas Kumar <vsrathod39@gmail.com>
2021-12-28 14:40:08 +0530

    counter final commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# React-State-Management-Asinmnt5185
- React state management
--- state-management/src/App.jsx
+++ state-management/src/App.jsx
@@ -1,21 +1,66 @@
-
-import Todo from './components/Todo'
+import {useState} from 'react';
+import ('./App.css')
 
 function App() {
-  return (
-    <>
-      <div>
-        <Todo heading = 'Mobile Operating System' data = {['Android', 'iPhone', 'Windows Phone', 'BalckBerry']}>
-          <p className='children-color'>we will add more soon...</p>
-        </Todo>
+  const [count, setCount] = useState(0);
+  const count_state = (value) => {
+    // if(count === 10 || count === -1){
+    //   setCount(0);
+    //   return;
+    // }else{
+    //   setCount(count + value);
+    // };
+
+    setCount((prev) => {
+      if(prev === 10 || prev === -1){
+        return 0
+      }
+    });
+
+    switch(value){
+      case 'A': setCount(count + 1); break;
+      case 'B': setCount(count - 1); break;
+      case 'C': setCount(count * 2); break;
+    };
+  };
+
+  // if(count > 9){
+  //   return (
+  //     <div className='App'>
+  //       <h3>Reached limit</h3>
+  //     </div>
+  //   );
+  // };
+
+  return <div className='App'>
+    <h3>{count}</h3>
+    {(count < 10 && count > -1) && (
+      <div className='button-Container'>
+        <button onClick={() => {count_state('A')}}>Add +1</button>
+        <button onClick={() => {count_state('B')}}>Sub -1</button>
+        <button onClick={() => {count_state('C')}}>Double *2</button>
       </div>
-      <div>
-        <Todo heading = 'Mobile Manufacturer' data = {['Samsung', 'HTC', 'Nokia', "Micromax", "Lava"]}>
-          <p className='children-color'>we will add more soon...</p>
-        </Todo>
-      </div>
-    </>
-  );
+    )}
+    {
+      count > 9 && (
+        <div className='button-Container'>
+          {
+            count > 10 && (setCount(10))
+          }
+          <h2>Reached max limit</h2>
+          <button onClick={() => {setCount(0)}}>Reset</button>
+        </div>
+      )
+    }
+    {
+      count < 0 && (
+        <div className='button-Container'>
+          <h2>Reached min limit</h2>
+          <button onClick={() => {setCount(0)}}>Reset</button>
+        </div>
+      )
+    }
+  </div>;
 }
 
 export default App;
